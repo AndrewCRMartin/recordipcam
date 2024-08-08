7fa3ae9ba9954eeefdb23f22c4739027ae449940
AndrewCRMartin <andrew@bioinf.org.uk>
2024-08-08 14:27:32 +0100

    Added killall script

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 . ./shed.cfg
 nohup ./recordipcam.pl shed.cfg &>$outputDir/recordip.log &
 
+. ./garden.cfg
+nohup ./recordipcam.pl garden.cfg &>$outputDir/recordip.log &
+
 #. ./sittingroom.cfg
 #nohup ./recordipcam.pl sittingroom.cfg &>$outputDir/recordip.log &
 
